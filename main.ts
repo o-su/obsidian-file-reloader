dc70997145323587c7a922c820646713d57cc556
o-su <xmbvztcxzc@email.cz>
2023-02-11 20:38:57 +0100

    formatted files

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 import { Plugin } from "obsidian";
 
 export default class MyPlugin extends Plugin {
-	async onload() {
-		this.registerEvent(
-			this.app.vault.on("modify", () => {
-				const recentLeaf = this.app.workspace.getMostRecentLeaf();
+  async onload() {
+    this.registerEvent(
+      this.app.vault.on("modify", () => {
+        const recentLeaf = this.app.workspace.getMostRecentLeaf();
 
-				if (recentLeaf) {
-					this.app.workspace.revealLeaf(recentLeaf);
-				}
-			})
-		);
-	}
+        if (recentLeaf) {
+          this.app.workspace.revealLeaf(recentLeaf);
+        }
+      })
+    );
+  }
 }
